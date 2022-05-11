b65f6ee3fdba988f7996db1a961977c291472704
B-du <bduroule101@gmail.com>
2022-05-11 17:30:12 +0200

    WIP

--- AlgorithmAndDataStructTest/Class1.cs
+++ AlgorithmAndDataStructTest/Class1.cs
@@ -7,6 +7,21 @@ namespace AlgorithmAndDataStructTest
     [TestFixture]
     public class Class1
     {
+        [SetUp]
+        public void Setup()
+        {
+     
+        }
+
+        [Test(Description = "Length")]
+        public void TestLength()
+        {
+            
+
+            /*foreach (var elem in referanceNumbers) {
+                Assert.AreEqual(elem, )
+            }*/
+        }
 
     }
 }
--- OrientedObjectProgramation/DesignPatern/Singleton.cs
+++ OrientedObjectProgramation/DesignPatern/Singleton.cs
@@ -1,49 +1,43 @@
-// tread safe 
-// lazy
-// lock free
+// class<out T, in R>
+// variance et contrevariance
 
+// Wakereferance
 
-// int[] Merge(int[] tabA, int[] tabB)  merge
+// di p 
+// factory 
+// SOLID
+
+namespace DesignPatern;
+
+public sealed class SingletonNotLazy<R> {  
+    SingletonNotLazy() {}  
+
+    private static readonly object lockObj = new object();  
+    private static R instance = null;  
+    public static R Instance {  
+        get {  
+            lock(lockObj) {  
+                if (instance == null) {  
+                    instance = new R();  
+                }  
+                return instance;  
+            }  
+        }  
+    }  
+} 
 
 public sealed class Singleton<R> where R: new()
 {
     private Singleton()
     {}
 
-    private static readonly Lazy<Singleton<R>> lazy = new Lazy<Singleton<R>>(() => new Singleton<R>()); 
-    public static Singleton<R> Instance    
+    private static readonly Lazy<R> lazy = new Lazy<R>(() => new R()); 
+    public static R Instance    
     {    
         get    
-        {    
+        {
             return lazy.Value;    
         }    
     }
-
 }
 
-public static class MergeTabInt
-{
-    public static int[] Merge(int[] tabA, int[] tabB)
-    {
-        int indexA = 0;
-        int indexB = 0;
-        int[] result = new int[tabA.Length + tabB.Length];
-
-        for (int i = 0; i < result.Length; i++) {
-            if (indexA < tabA.Length && indexB < tabB.Length) {
-                if (tabA[indexA] < tabB[indexB]) {
-                    result[i] = tabA[indexA++];
-                } else {
-                    result[i] = tabB[indexB++];
-                }
-            }
-            else if (indexA < tabA.Length) {
-                result[i] = tabA[indexA++];
-            }
-            else if (indexB < tabB.Length) {
-                result[i] = tabB[indexB++];
-            }
-        }
-        return result;
-    }
-}
